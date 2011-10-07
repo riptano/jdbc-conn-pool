29113d36edf256251675d0368718253fd2895ed9
patricioe <patricioe@gmail.com>
2011-10-06 17:06:35 -0700

    more tests

--- src/test/java/com/datastax/drivers/jdbc/pool/cassandra/connection/ConcurrentHClientPoolTest.java
+++ src/test/java/com/datastax/drivers/jdbc/pool/cassandra/connection/ConcurrentHClientPoolTest.java
@@ -1,11 +1,14 @@
 package com.datastax.drivers.jdbc.pool.cassandra.connection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.datastax.drivers.jdbc.pool.cassandra.BaseEmbededServerSetupTest;
+import com.datastax.drivers.jdbc.pool.cassandra.exceptions.HPoolExhaustedException;
 import com.datastax.drivers.jdbc.pool.cassandra.jdbc.CassandraConnectionHandle;
 
 public class ConcurrentHClientPoolTest extends BaseEmbededServerSetupTest {
@@ -16,6 +19,8 @@ public class ConcurrentHClientPoolTest extends BaseEmbededServerSetupTest {
   @Before
   public void setupTest() throws Exception {
     setupClient();
+    // Wait only one second.
+    cassandraHostConfigurator.setMaxWaitTimeWhenExhausted(500);
     cassandraHost = cassandraHostConfigurator.buildCassandraHosts()[0];
     clientPool = new ConcurrentHClientPool(cassandraHost);
   }
@@ -42,5 +47,28 @@ public class ConcurrentHClientPoolTest extends BaseEmbededServerSetupTest {
     assertEquals(1, clientPool.getNumActive());
     clientPool.releaseClient(conn);
     assertEquals(0, clientPool.getNumActive());
+  }
+  
+  @Test
+  public void testBorrowMoreThanActiveConnections() throws Exception {
+    CassandraConnectionHandle lastConn = null;
+    for (int i = 0; i < 50 ; i++)
+      lastConn = clientPool.borrowClient();
+
+    assertFalse(lastConn.isClosed());
+    assertEquals(50, clientPool.getNumActive());
+    
+    try {
+      clientPool.borrowClient();
+      fail("BorrowClient should throw an ExhaustedException here. Something went wrong.");
+    } catch (HPoolExhaustedException e) {
+      // Expected
+    }
+
+    clientPool.releaseClient(lastConn);
+
+    assertEquals(49, clientPool.getNumActive());
+
+    clientPool.borrowClient();
   }
 }
---
+++ src/test/java/com/datastax/drivers/jdbc/pool/cassandra/connection/HostTimeoutTrackerTest.java
@@ -0,0 +1,41 @@
+package com.datastax.drivers.jdbc.pool.cassandra.connection;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HostTimeoutTrackerTest {
+
+  private HostTimeoutTracker hostTimeoutTracker;
+
+  @Before
+  public void setup() {
+    // map cassandraHost --> arrayBlockingQueue of timestamp Longs
+    // - if abq.size > size, && peekLast.get > time, add host to excludedMap<cassandraHost,timestampOfExclusion> for <suspendTime> ms
+    // - background thread to sweep for exclusion time expiration every <sweepInterval> seconds
+    // - getExcludedHosts calls excludedMap.keySet
+    //
+    // HTL with a three timeout trigger durring 500ms intervals
+    CassandraHostConfigurator cassandraHostConfigurator = new CassandraHostConfigurator("localhost:9170");
+    cassandraHostConfigurator.setHostTimeoutCounter(3);
+    HConnectionManager connectionManager = new HConnectionManager("TestCluster", cassandraHostConfigurator);
+    hostTimeoutTracker = new HostTimeoutTracker(connectionManager, cassandraHostConfigurator);
+  }
+
+  @Test
+  public void testTrackHostLatency() {
+    CassandraHost cassandraHost = new CassandraHost("localhost:9170");
+    assertFalse(hostTimeoutTracker.checkTimeout(cassandraHost));
+    assertFalse(hostTimeoutTracker.checkTimeout(cassandraHost));
+    assertFalse(hostTimeoutTracker.checkTimeout(cassandraHost));
+    try {
+      Thread.currentThread().sleep(501);
+    } catch (InterruptedException e) {
+
+    }
+
+    assertTrue(hostTimeoutTracker.checkTimeout(cassandraHost));
+  }
+}
